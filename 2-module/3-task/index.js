e40614c2049a2d53899878b766d33571f944dc44
KonstantinNeb <kostyneb36@gmail.com>
2020-10-30 15:49:20 +0300

    (hw-2)

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
-};
+  sum: function() {
+    return this.a + this.b;
+  },
 
+  mul: function() {
+    return this.a * this.b;
+  },
+
+  read: function() {
+    this.a = (5);
+    this.b = (3);
+  }
+};
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
