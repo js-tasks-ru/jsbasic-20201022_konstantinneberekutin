625ea1a2cbadd18f09813fbb4d495a8ef305b328
KonstantinNeb <kostyneb36@gmail.com>
2020-11-07 09:50:14 +0300

    (hw-4)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,9 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+
+  ul.innerHTML = friends
+    .map(friend => `<li>${friend.firstName} ${friend.lastName}</li>`).join("");
+  return ul;
 }
