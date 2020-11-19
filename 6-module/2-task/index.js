29bb2e2c11148d4284a2e46f0b040c17f32753c9
KonstantinNeb <kostyneb36@gmail.com>
2020-11-19 20:55:40 +0300

    (hw-6)

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,6 +1,54 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
-  constructor(product) {
+  constructor({
+    name = '',
+    price = 0,
+    category = '',
+    image = false,
+    id = false
+  } = {}) {
+    this.name = name;
+    this.price = price;
+    this.category = category;
+    this.image = image;
+    this.id = id;
+
+    this.render();
+    this.addEventListener();
+  }
+
+  get template() {
+    return `
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
+          <span class="card__price">€${this.price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${this.name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    this.elem = createElement(this.template);
+  }
+
+  addEventListener() {
+    const addButton = this.elem.querySelector('.card__button');
+
+    addButton.addEventListener('click', () => {
+      const customEvent = new CustomEvent('product-add', {
+        detail: this.id,
+        bubbles: true
+      });
+
+      this.elem.dispatchEvent(customEvent);
+    });
   }
 }
